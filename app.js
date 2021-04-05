773f0c3e08592d9aebe80c50d1a6db39f229bb06
Andrei <kryvetskistud@gmail.com>
2021-04-05 22:19:30 +0300

    fix: change tags according to rule 1.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 //New task list item
 var createNewTaskElement=function(taskString){
 
-    var listItem=document.createElement("li");
+    var listItem=document.createElement("article");
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
